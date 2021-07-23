336e7d3d09473e45499ce133de1c5970e58edc9c
yorkox <49209840+yorkox0@users.noreply.github.com>
2021-07-23 11:01:18 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,53 @@
 import os, time
+from sys import stdout
+
+def red():
+    RED = "\033[1;31m"
+    stdout.write(RED)
+
+def green():
+    GREEN = "\033[0;32m"
+    stdout.write(GREEN)
+
+
+def blue():
+    BLUE = "\033[1;34m"
+    stdout.write(BLUE)
+
+def yellow():
+    YELLOW = "\033[1;33m"
+    stdout.write(YELLOW)
+
+def purple():
+    PURPLE = "\033[1;35m"
+    stdout.write(PURPLE)
+
+def white():
+    WHITE = "\033[1;37m"
+    stdout.write(WHITE)
+
+banner = """
+ █████╗ ██╗   ██╗████████╗ ██████╗ ██████╗ ███████╗██████╗ ██╗    ██╗███╗   ███╗
+██╔══██╗██║   ██║╚══██╔══╝██╔═══██╗██╔══██╗██╔════╝██╔══██╗██║    ██║████╗ ████║
+███████║██║   ██║   ██║   ██║   ██║██████╔╝███████╗██████╔╝██║ █╗ ██║██╔████╔██║
+██╔══██║██║   ██║   ██║   ██║   ██║██╔══██╗╚════██║██╔═══╝ ██║███╗██║██║╚██╔╝██║  (by Yorkox)
+██║  ██║╚██████╔╝   ██║   ╚██████╔╝██████╔╝███████║██║     ╚███╔███╔╝██║ ╚═╝ ██║
+╚═╝  ╚═╝ ╚═════╝    ╚═╝    ╚═════╝ ╚═════╝ ╚══════╝╚═╝      ╚══╝╚══╝ ╚═╝     ╚═╝
+"""
 
 def menu():
+    red()
+    print(banner)
+    blue()
+    time.sleep(1)
     print("1 -> Instalar Requerimientos necesarios")
+    time.sleep(1)
     print("\n2 -> Instalar bspwm")
+    time.sleep(1)
     print("\n3 -> Instalar Polybar")
+    time.sleep(1)
     print("\n4 -> Salir")
+    time.sleep(1)
 
     option = input("\n-->> ")
 
@@ -18,6 +61,7 @@ def menu():
         exit()
 
 def req():
+    green()
     print("[+] Instalando requerimientos...\n")
 
     # Instalando Requerimientos
@@ -32,25 +76,41 @@ def req():
         menu()
 
 def bspwm():
-    # Instalando bspwm
+    green()
+
+    # Clona la repo de bspwm
     os.system("git clone https://github.com/baskerville/bspwm.git")
     os.system("mv bspwm/* .")
-    os.system("rm -r bspwm/")
+    os.system("sudo rm -r bspwm/")
     os.system("make")
+
+    # Acava del build
     os.system("sudo make install")
-    os.system("rm -r artworks/ contrib/ doc/ src/ tests/ bspc bspc.o bspwm bspwm.o desktop.o events.o ewmh.o geometry.o helpers.o history.o jsmn.o LICENSE Makefile messages.o monitor.o parse.o pointer.o query.o README.md restore.o rule.o settings.o Sourcedeps stack.o subscribe.o tree.o VERSION window.o")
+
+    # Elimina los archivos de bspwm
+    os.system("sudo rm -r artworks/ contrib/ doc/ src/ tests/ bspc bspc.o bspwm bspwm.o desktop.o events.o ewmh.o geometry.o helpers.o history.o jsmn.o LICENSE Makefile messages.o monitor.o parse.o pointer.o query.o README.md restore.o rule.o settings.o Sourcedeps stack.o subscribe.o tree.o VERSION window.o")
+
+    # Clona la repo de sxhkd
     os.system("git clone https://github.com/baskerville/sxhkd.git")
     os.system("mv sxhkd/* .")
-    os.system("rm -r sxhkd/")
+    os.system("sudo rm -r sxhkd/")
     os.system("cd ../sxhkd")
     os.system("make")
+
+    # Acaba el build
     os.system("sudo make install")
+
+    # Crea las carpetas de bspwm y sxhkd en ~/.config
     os.system("mkdir ~/.config/bspwm")
     os.system("mkdir ~/.config/sxhkd")
     os.system("cp examples/bspwmrc ~/.config/bspwm/")
+
+    # Les da permisos de ejecucion a bspwmrc
     os.system("chmod +x ~/.config/bspwm/bspwmrc")
     os.system("cp examples/sxhkdrc ~/.config/sxhkd/")
-    os.system("rm -r contrib/ doc/ examples/ src/ grab.o helpers.o LICENSE Makefile parse.o README.md Sourcedeps sxhkd sxhkd.o types.o VERSION")
+
+    # Elimina los archivos de sxhkd
+    os.system("sudo rm -r contrib/ doc/ examples/ src/ grab.o helpers.o LICENSE Makefile parse.o README.md Sourcedeps sxhkd sxhkd.o types.o VERSION")
     os.system("cp tools/sxhkdrc ~/.config/sxhkd")
     print("\n[+] Bspwm instalado correctamente!")
 
@@ -58,54 +118,84 @@ def bspwm():
         menu()
 
 def polybar():
+    green()
+
+    # Clona el repo de polybar
     os.system("git clone --recursive https://github.com/polybar/polybar")
     os.system("mv polybar/* .")
-    os.system("rm -r polybar/")
+    os.system("sudo rm -r polybar/")
     os.system("cmake .")
     os.system("make -j$(nproc)")
-    os.system("sudo make install")
-    os.system("rm -r bin/ cmake/ CMakeFiles/ common/ config/ contrib/ doc/ generated-sources/ include/ lib/ libs/ polybar/ src/ tests/ banner.png build.sh CHANGELOG.md CMajeCache.txt cmake_install.cmake CMakeLists.txt compile_commands.json CONTRIBUTING.md install_manifest LICENSE Makefile README.md SUPPORT.md version.txt")
 
+    # Acaba el build
+    os.system("sudo make install")
+
+    # Elimina los archivos de polybar
+    os.system("sudo rm -r bin/ cmake/ CMakeFiles/ common/ config/ contrib/ doc/ generated-sources/ include/ lib/ libs/ polybar/ src/ tests/ banner.png build.sh CHANGELOG.md CMajeCache.txt cmake_install.cmake CMakeLists.txt compile_commands.json CONTRIBUTING.md install_manifest LICENSE Makefile README.md SUPPORT.md version.txt")
+
+    # Clona el repo de picom
     os.system("git clone https://github.com/ibhagwan/picom.git")
     os.system("mv picom/* .")
-    os.system("rm -r picom/")
+    os.system("sudo rm -r picom/")
     os.system("git submodule update --init --recursive")
     os.system("meson --buildtype=release . build")
     os.system("ninja -C build")
+
+    # Hace el build de picom
     os.system("sudo ninja -C build install")
-    os.system("rm -r *.md *.conf *.desktop *.txt *.build *.spdx *.glsl COPYING Doxyfile CONTRIBUTORS bin/ build/ dbus-examples/ LICENSES/ man/ media/ meson/ src/ subprojects/ tests/")
+
+    # Elimina los archivos de picom
+    os.system("sudo rm -r *.md *.conf *.desktop *.txt *.build *.spdx *.glsl COPYING Doxyfile CONTRIBUTORS bin/ build/ dbus-examples/ LICENSES/ man/ media/ meson/ src/ subprojects/ tests/")
+
+    # Añade el wallpaper
     os.system("mkdir ~/.wallpapers")
     os.system("mv tools/wallpaper.jpg ~/.wallpapers")
     os.system("echo 'feh --bg-fill ~/.wallpapers/wallpaper.jpg' >> ~/.config/bspwm/bspwmrc")
+
+    # Clona el tema de blue-sky
     os.system("git clone https://github.com/VaughnValle/blue-sky.git")
     os.system("mkdir ~/.config/polybar")
+
+    # Copia el tema de blue-sky a la config de polybar
     os.system("cp -r blue-sky/polybar/* ~/.config/polybar")
     os.system("echo '~/.config/polybar/./launch.sh' >> ~/.config/bspwm/bspwmrc")
     os.system("sudo cp blue-sky/polybar/fonts/* /usr/share/fonts/truetype")
     os.system("fc-cache -v")
+
+    # Copia la config de picom
     os.system("mkdir ~/.config/picom")
     os.system("cp blue-sky/picom.conf ~/.config/picom")
     os.system("echo 'bspc config focus_follows_pointer true' >> ~/.config/bspwm/bspwmrc")
     os.system("echo 'picom --experimental-backends &' >> ~/.config/bspwm/bspwmrc")
     os.system("echo 'bspc config border_width 0' >> ~/.config/bspwm/bspwmrc")
     os.system("mkdir ~/.config/bin")
+
+    # Mete el ethernet_status.sh, hackthebox_status.sh, target_to_hack.sh y target en ~/.config/bin
     os.system("wget https://raw.githubusercontent.com/LevisWings/Auto-PWE/main/ethernet_status.sh")
-    os.system("chmod +x ethernet_status.sh")
+    os.system("chmod +x ethernet_status.sh 2>/dev/null")
     os.system("mv ethernet_status.sh ~/.config/bin")
     os.system("wget https://raw.githubusercontent.com/LevisWings/Auto-PWE/main/hackthebox.sh")
-    os.system("chmod +x ethernet_status.sh")
+    os.system("chmod +x hackthebox.sh")
     os.system("mv hackthebox.sh ~/.config/bin")
     os.system("cp tools/target_to_hack.sh .")
     os.system("chmod +x target_to_hack.sh")
     os.system("mv target_to_hack.sh ~/.config/bin")
     os.system("echo '' > ~/.config/bin/target")
+
+    # Copia la config de polybar personalizada
     os.system("cp tools/launch.sh ~/.config/polybar")
     os.system("cp tools/current.ini ~/.config/polybar")
+
+    # Copia la config de rofi personalizada
     os.system("mkdir ~/.config/rofi")
     os.system("mkdir ~/.config/rofi/themes")
     os.system("cp blue-sky/nord.rasi ~/.config/rofi/themes")
+
+    # Mueve los comandos settarget y cleartarget a /bin
     os.system("sudo cp tools/settarget /bin")
     os.system("sudo cp tools/cleartarget /bin")
+    os.system("sudo chmod +x /bin/settarget")
+    os.system("sudo chmod +x /bin/cleartarget")
 
     print("\n[+] POLYBAR INSTALADO!!!")
 if __name__ == '__main__':
