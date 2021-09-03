53df99f485db1eae94727e5af708fa0b819b5d26
yorkox <49209840+yorkox0@users.noreply.github.com>
2021-09-03 02:10:56 +0200

    Add files via upload

--- tools/target_to_hack.sh
+++ tools/target_to_hack.sh
@@ -1,9 +1,30 @@
-#!/bin/sh
-
-target=$(cat ~/.config/bin/target)
+#!/bin/bash
+ 
+ # Mode of use: settarget <IP> <Name> or only settarget <Ip>
+ 
+target=$(/usr/bin/cat ~/.config/bin/target | awk '{print $1}')
+name=$(/usr/bin/cat ~/.config/bin/target | awk '{print $2}')
 
 if [ $target ]; then
-	echo "%{F#e51d0b} %{F#ffffff}$target%{u-}"
+ 
+        status=$(/usr/bin/ping -c 1 $target | /usr/bin/grep "received" | /usr/bin/awk '{print $4}')
+ 
+        if [ $status -ne 1 ]; then
+                echo "%{F#FF0000} %{F#FFFFFF}$target $name%{u-}"
+        else
+            ttl=$(/usr/bin/ping -c 1 $target | /usr/bin/grep "ttl" | /usr/bin/awk '{print $6}' | /usr/bin/awk '{print $2}' FS==)
+ 
+            if [ $ttl -le 64 ]; then
+                echo "%{F#FF0000} %{F#FFFFFF}$target - $name%{u-}"
+            elif [ $ttl -ge 65 -a $ttl -le 128 ]; then
+                echo "%{F#FF0000} %{F#FFFFFF}$target - $name%{u-}"
+            elif [ $ttl -ge 128 -a $ttl -le 254 ]; then
+                echo "%{F#FF0000} %{F#FFFFFF}$target - $name%{u-}"
+            else
+                echo "%{F#FF0000}? %{F#FFFFFF}$target - $name%{u-}"
+            fi
+        fi
 else
-	echo "%{F#e51d0b}%{u-}%{F#ffffff} No target"
+    #echo "%{F#3B83BD}什 %{F#FFFFFF}No target%{u-}"
+    echo "%{F#e51d0b}%{u-}%{F#ffffff} No target"
 fi
