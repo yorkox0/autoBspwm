31b1aa6052e4ade2906467436b163569f5f02bf2
yorkox <49209840+yorkox0@users.noreply.github.com>
2021-09-27 18:39:34 +0200

    Update target_to_hack.sh

--- tools/target_to_hack.sh
+++ tools/target_to_hack.sh
@@ -1,29 +1,9 @@
-#!/bin/bash
- 
- # Mode of use: settarget <IP> <Name> or only settarget <Ip>
- 
-target=$(/usr/bin/cat ~/.config/bin/target | awk '{print $1}')
-name=$(/usr/bin/cat ~/.config/bin/target | awk '{print $2}')
+#!/bin/sh
+
+target=$(cat /home/$USER/.config/bin/target)
 
 if [ $target ]; then
- 
-        status=$(/usr/bin/ping -c 1 $target | /usr/bin/grep "received" | /usr/bin/awk '{print $4}')
- 
-        if [ $status -ne 1 ]; then
-                echo "%{F#FF0000} %{F#FFFFFF}$target $name%{u-}"
-        else
-            ttl=$(/usr/bin/ping -c 1 $target | /usr/bin/grep "ttl" | /usr/bin/awk '{print $6}' | /usr/bin/awk '{print $2}' FS==)
- 
-            if [ $ttl -le 64 ]; then
-                echo "%{F#FF0000} %{F#FFFFFF}$target - $name%{u-}"
-            elif [ $ttl -ge 65 -a $ttl -le 128 ]; then
-                echo "%{F#FF0000} %{F#FFFFFF}$target - $name%{u-}"
-            elif [ $ttl -ge 128 -a $ttl -le 254 ]; then
-                echo "%{F#FF0000} %{F#FFFFFF}$target - $name%{u-}"
-            else
-                echo "%{F#FF0000}? %{F#FFFFFF}$target - $name%{u-}"
-            fi
-        fi
+    echo "%{F#e51d0b}什%{F#ffffff} $target%{u-}"
 else
-    echo "%{F#e51d0b}%{u-}%{F#ffffff} No target"
+    echo "%{F#e51d0b}ﲅ %{u-}%{F#ffffff} No target"
 fi
