6302021f28d28c6806131fdc6529059e17567611
yorkox <49209840+yorkox0@users.noreply.github.com>
2024-03-04 07:34:25 +0100

    Error Picom
    Se ha reparado el error al instalar picom que no funcionaba, ahora se instala por repositorio automaticamente y no hace falta hacer el build.

--- main.py
+++ main.py
@@ -74,7 +74,7 @@ def req():
     os.system("sudo apt install net-tools libuv1-dev build-essential git vim xcb libxcb-util0-dev libxcb-ewmh-dev libxcb-randr0-dev libxcb-icccm4-dev libxcb-keysyms1-dev libxcb-xinerama0-dev libasound2-dev libxcb-xtest0-dev libxcb-shape0-dev -y")
     os.system("sudo apt install cmake cmake-data pkg-config python3-sphinx libcairo2-dev libxcb1-dev libxcb-util0-dev libxcb-randr0-dev libxcb-composite0-dev python3-xcbgen xcb-proto libxcb-image0-dev libxcb-ewmh-dev libxcb-icccm4-dev libxcb-xkb-dev libxcb-xrm-dev libxcb-cursor-dev libasound2-dev libpulse-dev libjsoncpp-dev libmpdclient-dev libcurl4-openssl-dev libnl-genl-3-dev -y")
     os.system("sudo apt install meson libxext-dev libxcb1-dev libxcb-damage0-dev libxcb-xfixes0-dev libxcb-shape0-dev libxcb-render-util0-dev libxcb-render0-dev libxcb-randr0-dev libxcb-composite0-dev libxcb-image0-dev libxcb-present-dev libxcb-xinerama0-dev libpixman-1-dev libdbus-1-dev libconfig-dev libgl1-mesa-dev libpcre2-dev libevdev-dev uthash-dev libev-dev libx11-xcb-dev libxcb-glx0-dev -y")
-    os.system("sudo apt install bspwm rofi caja feh gnome-terminal scrot neovim xclip tmux acpi scrub bat wmname -y")
+    os.system("sudo apt install picom bspwm rofi caja feh gnome-terminal scrot neovim xclip tmux acpi scrub bat wmname -y")
 
     time.sleep(2)
     print("[+] Requetimientos instalados correctamente")
@@ -188,7 +188,7 @@ def polybar():
     os.system("echo 'bspc config border_width 0' >> ~/.config/bspwm/bspwmrc")
     os.system("mkdir ~/.config/bin")
     os.system("echo 'picom &' >> ~/.config/bspwm/bspwmrc")
-    os.system("setxkbmap es")
+    # os.system("setxkbmap es")
 
     # Instalacion de Fuentes para Polybar
     os.system("sudo cp ~/.config/polybar/fonts/* /usr/share/fonts")
